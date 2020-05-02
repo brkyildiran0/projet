f86a00a2f75df65ba6f04fae10251d20c386434a
celalberkecan <64156360+celalberkecan@users.noreply.github.com>
2020-05-02 21:40:18 +0300

    Login - Register

--- projet/app/src/main/java/com/cs102/projet/MainActivity.java
+++ projet/app/src/main/java/com/cs102/projet/MainActivity.java
@@ -3,13 +3,68 @@ package com.cs102.projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    Button signUpButton, loginButton;
+    EditText userNameText;
+    EditText passwordText;
+    DatabaseHelper databaseHelper;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // database...
+        databaseHelper = new DatabaseHelper(this);
+
+        //Button's id..
+        signUpButton = (Button)findViewById(R.id.signUpButton);
+        loginButton = (Button)findViewById(R.id.loginButton);
+
+        // EditText's id...
+        userNameText = (EditText)findViewById(R.id.userName) ;
+        passwordText = (EditText)findViewById(R.id.password) ;
+
+
+        // Click Listener For Buttons...
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = userNameText.getText().toString().trim();
+                String password = passwordText.getText().toString().trim();
+                if (databaseHelper.isLoginValid(userName, password)){
+                    Intent intent = new Intent(MainActivity.this,MyProjects.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this,"login successful",Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(MainActivity.this,"Invalid", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = userNameText.getText().toString().trim();
+                String password = passwordText.getText().toString().trim();
+
+                goNextPage();
+
+            }
+        });
+    }
+    private void goNextPage(){
+        Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
---
+++ projet/app/src/main/java/com/cs102/projet/RegisterActivity.java
@@ -0,0 +1,77 @@
+package com.cs102.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button completedButton, createUser;
+    EditText confirmText, emailInput, userNameInput, passwordInput, passwordCheck;
+
+    DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Database..
+        databaseHelper = new DatabaseHelper(this);
+
+        // Button's Id...
+        completedButton = (Button)findViewById(R.id.completedButton);
+        createUser = (Button)findViewById(R.id.createUser);
+
+        // EditText's Id...
+        confirmText = (EditText)findViewById(R.id.confirmationCode);
+        userNameInput = (EditText)findViewById(R.id.userNameInput);
+        emailInput = (EditText)findViewById(R.id.emailInput);
+        passwordInput = (EditText)findViewById(R.id.passwordInput);
+        passwordCheck = (EditText)findViewById(R.id.passwordCheck);
+
+
+
+        createUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this,"confirmation code has been send", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        completedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userNameValue = userNameInput.getText().toString().trim();
+                String emailValue = emailInput.getText().toString();
+                String passwordValue = passwordInput.getText().toString();
+                String passwordCheckValue = passwordCheck.getText().toString();
+
+                if (userNameValue.length() > 1){
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put("username",userNameValue);
+                    contentValues.put("email",emailValue);
+                    contentValues.put("password", passwordValue);
+
+                    databaseHelper.createUser(contentValues);
+                    goLoginPage();
+                    Toast.makeText(RegisterActivity.this, "User Registered", Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(RegisterActivity.this, "Invalid", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    private void goLoginPage(){
+        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+}

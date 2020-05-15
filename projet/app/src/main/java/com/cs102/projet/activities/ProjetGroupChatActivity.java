66c9588dfca3f77bff507affeba7f378aac4a1c7
Burak Yıldıran <burak.yildiran@ug.ihsandogramacibilkentuniversitesi.com>
2020-05-15 17:57:50 +0300

    Added Group Chat button to the ProJet page Added Group Chat Activity

---
+++ projet/app/src/main/java/com/cs102/projet/activities/ProjetGroupChatActivity.java
@@ -0,0 +1,17 @@
+package com.cs102.projet.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cs102.projet.R;
+
+public class ProjetGroupChatActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projet_group_chat);
+    }
+}

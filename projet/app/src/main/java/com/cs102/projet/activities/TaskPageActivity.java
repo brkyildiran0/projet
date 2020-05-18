f60de51aa3c54902c5d5d237aaece48d22617034
Burak Yıldıran <burak.yildiran@ug.ihsandogramacibilkentuniversitesi.com>
2020-05-18 20:34:11 +0300

    Task Page Activity Toolbar removed (was unnecessary)

--- projet/app/src/main/java/com/cs102/projet/activities/TaskPageActivity.java
+++ projet/app/src/main/java/com/cs102/projet/activities/TaskPageActivity.java
@@ -70,33 +70,4 @@ public class TaskPageActivity extends AppCompatActivity
         super.onStop();
         adapter.stopListening();
     }
-
-    //Method for the AppBar Buttons & Icons
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.create_new_projet_menu, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.addNewMember:
-                startActivity(new Intent(getApplicationContext(), AddMemberActivity.class));
-                return true;
-            case R.id.addNewTask:
-                startActivity(new Intent(getApplicationContext(), AddTaskActivity.class));
-                return true;
-            case R.id.leaveProjet:
-                //TODO will be handled later
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }

cf0b6bd6483f23f27bba495c1c59f5cabde65f9d
Burak Yıldıran <burak.yildiran@ug.ihsandogramacibilkentuniversitesi.com>
2020-05-19 13:41:50 +0300

    Settings page is now functional Dark mode is now functional

--- projet/app/src/main/java/com/cs102/projet/activities/SettingsActivity.java
+++ projet/app/src/main/java/com/cs102/projet/activities/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.cs102.projet.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
@@ -10,13 +11,19 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.cs102.projet.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity
 {
@@ -50,7 +57,63 @@ public class SettingsActivity extends AppCompatActivity
         enableDarkMode = findViewById(R.id.enableDarkMode);
         disableDarkMode = findViewById(R.id.disableDarkMode);
 
+        //Handling user's name change request
+        buttonChangeName.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Handling empty input
+                if (!newNameInput.getText().toString().equals(""))
+                {
+                    String newName = newNameInput.getText().toString();
 
+                    Map<String, String > userNewName = new HashMap<>();
+                    userNewName.put("user_name", newName);
+                    database.collection("Users").document(currentUserMail).set(userNewName, SetOptions.merge());
 
+                    newNameInput.setText("");
+                    Toast.makeText(SettingsActivity.this, "Name changed!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(SettingsActivity.this, "Please enter a name first!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        enableDarkMode.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Updating user's preference at database
+                Map<String, Boolean> userPrefUpdate = new HashMap<>();
+                userPrefUpdate.put("user_darkmode_preference", true);
+                database.collection("Users").document(currentUserMail).set(userPrefUpdate,SetOptions.merge());
+
+                //Updating the app to darkmode immediately
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                finish();
+            }
+        });
+
+        disableDarkMode.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Updating user's preference at database
+                Map<String, Boolean> userPrefUpdate = new HashMap<>();
+                userPrefUpdate.put("user_darkmode_preference", false);
+                database.collection("Users").document(currentUserMail).set(userPrefUpdate,SetOptions.merge());
+
+                //Updating the app to darkmode immediately
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                finish();
+            }
+        });
     }
 }

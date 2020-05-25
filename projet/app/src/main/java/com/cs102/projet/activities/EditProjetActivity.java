007f5c6e444fd0644d20f6a035bfda9694204027
Burak Yıldıran <burak.yildiran@ug.ihsandogramacibilkentuniversitesi.com>
2020-05-25 18:20:38 +0300

    Edit Projet page code cleanup

--- projet/app/src/main/java/com/cs102/projet/activities/EditProjetActivity.java
+++ projet/app/src/main/java/com/cs102/projet/activities/EditProjetActivity.java
@@ -1,13 +1,9 @@
 package com.cs102.projet.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -15,41 +11,36 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.cs102.projet.R;
-import com.cs102.projet.classes.MyNotificationClass;
-import com.cs102.projet.interfaces.GetInformations;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class EditProjetActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
-    FirebaseFirestore database;
-    FirebaseAuth myFirebaseAuth;
-    FirebaseUser currentUser;
-    private EditText editTextProjetDesc;
-    private EditText editTextProjetDueDate;
-    private EditText editTextProjetDueHour;
+
     String projetName;
     String currentUserMail;
     Button buttonApplyChanges;
     DocumentReference projetReference;
-    MyNotificationClass myNotification;
+    EditText editTextProjetDesc;
+    EditText editTextProjetDueDate;
+    EditText editTextProjetDueHour;
+    FirebaseFirestore database;
+    FirebaseAuth myFirebaseAuth;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -71,29 +62,31 @@ public class EditProjetActivity extends AppCompatActivity implements DatePickerD
         assert currentUser != null;
         currentUserMail = currentUser.getEmail();
 
-        //connecting views to code
+        //Connecting views to code
         buttonApplyChanges = findViewById(R.id.buttonApplyChanges);
         editTextProjetDesc = findViewById(R.id.editProjetDesc);
         editTextProjetDueDate = findViewById(R.id.editProjetDueDate);
         editTextProjetDueHour = findViewById(R.id.editProjetDueHour);
 
-        //Declaring the current projet's reference for code clarity later on
+        //Declaring the current ProJet's reference for code clarity later on
         projetReference = database.collection("ProJets").document(projetName);
 
-        // Using DatePicker in order to get valid dates
-        // Date ClickListener..
-        editTextProjetDueDate.setOnClickListener(new View.OnClickListener() {
+        //On click listener for picking date
+        editTextProjetDueDate.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 showDatePickerDialog();
             }
         });
 
-        // Using TimePicker in order to get valid times..
-        // Hour ClickListener...
-        editTextProjetDueHour.setOnClickListener(new View.OnClickListener() {
+        //On click listener for picking time
+        editTextProjetDueHour.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 showTimePickerDialog();
             }
         });
@@ -110,7 +103,7 @@ public class EditProjetActivity extends AppCompatActivity implements DatePickerD
             }
         });
 
-        //on click listener of Apply Changes button
+        //On click listener of Apply Changes button
         buttonApplyChanges.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -120,10 +113,9 @@ public class EditProjetActivity extends AppCompatActivity implements DatePickerD
                 if (editTextProjetDesc.getText().toString().equals("") || editTextProjetDueDate.getText().toString().equals("") || editTextProjetDueHour.getText().toString().equals(""))
                 {
                     Toast.makeText(EditProjetActivity.this, "Please fill in all fields!", Toast.LENGTH_SHORT).show();
-                }
-                else
+                } else
                 {
-                    //Editing the projet properties according to the new input
+                    //Editing the ProJet properties according to the new input
                     Map<String, String> projetInfo = new HashMap<>();
                     projetInfo.put("projet_desc", editTextProjetDesc.getText().toString());
                     projetInfo.put("projet_due_date", editTextProjetDueDate.getText().toString());
@@ -154,7 +146,11 @@ public class EditProjetActivity extends AppCompatActivity implements DatePickerD
             }
         });
     }
-    // Method For Date Picker to get valid dates from users..
+
+    /**
+     * This method is called whenever due date dialog is clicked
+     * Pops-up the dialog for user to pick the date
+     */
     public void showDatePickerDialog()
     {
         DatePickerDialog datePickerDialog = new DatePickerDialog(
@@ -169,15 +165,27 @@ public class EditProjetActivity extends AppCompatActivity implements DatePickerD
 
     }
 
+    /**
+     * Helper method for showDatePickerDialog() to work properly, and set the given input as a String.
+     *
+     * @param view       the pop-up view for user to input the date
+     * @param year       user input
+     * @param month      user input
+     * @param dayOfMonth user input
+     */
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
     {
+        //Because calendar starts at 0 for counting the months, incrementing month by 1
         month++;
         String date = dayOfMonth + "/" + month + "/" + year;
         editTextProjetDueDate.setText(date);
     }
 
-    // Method For Time Picker to get valid times from users..
+    /**
+     * This method is called whenever due time dialog is clicked
+     * Pops-up the dialog for user to pick the time
+     */
     public void showTimePickerDialog()
     {
         TimePickerDialog timePickerDialog = new TimePickerDialog(
@@ -189,34 +197,17 @@ public class EditProjetActivity extends AppCompatActivity implements DatePickerD
         timePickerDialog.show();
     }
 
+    /**
+     * Helper method for showTimePickerDialog() to work properly, and set the given input as a String.
+     *
+     * @param view      the pop-up view for user to input the time
+     * @param hourOfDay user input
+     * @param minute    user input
+     */
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute)
     {
-
-        editTextProjetDueHour.setText(hourOfDay + ":" +minute);
-    }
-
-    // The OnCompleteListener method is an asynchronous method. So, we cannot move the informations outside of the method.
-    // To do that, we create a new method that takes parameters an interface called "getInformations" and a query that we want to implement this method on.
-    // What does this method do? It is a normal method until the line "getInformations.useInfo(eventlist)".
-    // While this method is being used, the inner method is already completed. So, we can get the informations and we use them in "useInfo" method.
-    public void moveData(final GetInformations getInformations, Query query) {
-        query.get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            List<String> eventList = new ArrayList<>();
-
-                            for(DocumentSnapshot doc : task.getResult()) {
-                                String e = doc.getId();
-                                eventList.add(e);
-                            }
-                            getInformations.useInfo(eventList);
-                        } else {
-                            Log.e("QuerySnapshot Error!", "There is a problem while getting documents!");
-                        }
-                    }
-                });
+        String assigner = hourOfDay + ":" + minute;
+        editTextProjetDueHour.setText(assigner);
     }
 }

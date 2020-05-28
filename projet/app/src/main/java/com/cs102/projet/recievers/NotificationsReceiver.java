09c11cc761a08fedb301c4a7341dabd48734eae5
Burak Yıldıran <burak.yildiran@ug.ihsandogramacibilkentuniversitesi.com>
2020-05-28 14:11:06 +0300

    Receiver code clean up

--- projet/app/src/main/java/com/cs102/projet/recievers/NotificationsReceiver.java
+++ projet/app/src/main/java/com/cs102/projet/recievers/NotificationsReceiver.java
@@ -8,14 +8,13 @@ import android.content.Intent;
 
 import com.cs102.projet.classes.NotificationPage;
 
-public class NotificationsReceiver extends BroadcastReceiver {
-
+public class NotificationsReceiver extends BroadcastReceiver
+{
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent)
+    {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
         Notification notification = intent.getParcelableExtra("object");
-
         notificationManager.notify(1,notification);
     }
 }
